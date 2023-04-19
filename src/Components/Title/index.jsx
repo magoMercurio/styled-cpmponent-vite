094154799294d3d03fa21f602cfa6d56c5f405bb
mago_d_Mercurio <davila.feria@gmail.com>
2023-04-19 14:42:25 -0500

    Title styled

--- src/Components/Title/index.jsx
+++ src/Components/Title/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import styled from 'styled-components'
 
-const Title = ({ children }) => {
+const Title = styled.h1`
+  color: grey;
+  padding: 25px 0;
+`
+
+/* const Title = ({ children }) => {
   return <h1 className="title">{children}</h1>;
-};
+}; */
 export default Title;
